59ffe83177a00db13d080a16b3ffe473c30248d5
chriswebdv <chriswebdv@gmail.com>
2024-05-26 16:44:07 -0600

    Create 2 boolean variables to keep track of the winner and loser

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 let firstCard = Math.floor(Math.random() * 10) + 2;
 let secondCard = Math.floor(Math.random() * 10) + 2;
+let hasBlackJack = false;
+let isAlive = true;
 
 let sum = firstCard + secondCard;
 
 if (sum <= 20) {
-  console.table("Get another card 😀");
+  console.log("Get another card 😀");
 } else if (sum === 21) {
   console.log("Winner winner chicken dinner! 😁");
+  hasBlackJack = true;
 } else {
   console.log("Snooze you lose! 🗿");
+  isAlive = false;
 }
+
+console.log(hasBlackJack);
+console.log(isAlive);

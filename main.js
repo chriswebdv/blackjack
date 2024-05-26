4e96b1d7015492da5198f42b79dc0a487df23a8d
chriswebdv <chriswebdv@gmail.com>
2024-05-26 13:46:52 -0600

    Create an if statement to see output if the players wins, loses or is out of the game

--- main.js
+++ main.js
@@ -2,4 +2,11 @@ let firstCard = Math.floor(Math.random() * 10) + 2;
 let secondCard = Math.floor(Math.random() * 10) + 2;
 
 let sum = firstCard + secondCard;
-console.log(sum);
+
+if (sum < 21) {
+  console.table("Get another card 😀");
+} else if (sum === 21) {
+  console.log("Winner winner chicken dinner! 😁");
+} else {
+  console.log("Snooze you lose! 🗿");
+}

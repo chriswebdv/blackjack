7255726299e53feb63257bb08a6dd8479e9075be
chriswebdv <chriswebdv@gmail.com>
2024-05-26 16:35:07 -0600

    Made slight chage on my if condition

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let secondCard = Math.floor(Math.random() * 10) + 2;
 
 let sum = firstCard + secondCard;
 
-if (sum < 21) {
+if (sum <= 20) {
   console.table("Get another card 😀");
 } else if (sum === 21) {
   console.log("Winner winner chicken dinner! 😁");

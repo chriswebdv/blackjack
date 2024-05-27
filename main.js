6b3eb2bf8e1713079fe2d9b9c0b462f13f9c2431
chriswebdv <chriswebdv@gmail.com>
2024-05-27 07:52:15 -0600

    remove log function and add textContent to display the end user message on the app

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 let firstCard = Math.floor(Math.random() * 10) + 2;
 let secondCard = Math.floor(Math.random() * 10) + 2;
-let sum = firstCard + secondCard;
+let sum = firstCard + secondCard + 5;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+let messageEl = document.getElementById("message-el");
 
 let startGame = () => {
   if (sum <= 20) {
-    message += "Get another card 😀";
+    message = "Get another card 😀";
   } else if (sum === 21) {
-    message += "Winner winner chicken dinner! 😁";
+    message = "Winner winner chicken dinner! 😁";
     hasBlackJack = true;
   } else {
-    message += "Snooze you lose! 🗿";
+    message = "Snooze you lose! 🗿";
     isAlive = false;
   }
 
   console.log(message);
+  messageEl.textContent = message;
 };
